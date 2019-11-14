f9fb4b3584e709267a8176c5a70b617de2d96fd3
callicerbotx <mrbubbaloo69@gmail.com>
2019-11-14 02:06:47 -0600

    Validaciones bien direccionada

--- vista/piePaginaEIPublico.php
+++ vista/piePaginaEIPublico.php
@@ -8,8 +8,8 @@
 </footer>
 
 <!-- Validaciones -->
-<script src="VISTA/js/valida_login.js"></script>
-<script src="VISTA/js/valida_registro.js"></script>
+<script src="assets/js/valida_login.js"></script>
+<script src="assets/js/valida_registro.js"></script>
   
 <script src="assets/js/funcionTabs.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
